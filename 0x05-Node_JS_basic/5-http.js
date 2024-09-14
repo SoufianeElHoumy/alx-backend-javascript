b503e2374d3df7f4c73df9ec0a51e7aa052e4bdc
SoufianeElHoumy <142119581+SoufianeElHoumy@users.noreply.github.com>
2024-09-14 13:08:38 +0000

    NodeJS Basics by Soufiane El Houmy

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -9,7 +9,7 @@ const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Soufiane El Houmy <https://github.com/SoufianeElHoumy>
  */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
